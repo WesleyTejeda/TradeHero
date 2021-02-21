0c51e0f280aed2e907ae8eb70dc138251bf9426a
Wesley PC <wesley.tejeda95@gmail.com>
2021-02-21 17:26:03 -0500

    Updated user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.SchemaType({
+const userSchema = new Schema({
     firstName: {
         type: String,
         required: true
